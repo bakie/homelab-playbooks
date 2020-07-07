802c471f7b7c080b2ab1a8d0094e85b34035ff66
Bakie <bakelants.developer@gmail.com>
2020-07-07 20:12:08 +0200

    added role grafana

---
+++ roles/grafana/README.md
@@ -0,0 +1,26 @@
+## Role Variables
+
+### grafana_url
+The url for grafana which is used in the apache vhost
+
+Default value is "{{ inventory_hostname }}.local"
+
+### grafana_admin_user
+The username for the grafana login
+
+Default value is "grafana"
+
+###grafana_admin_password
+The password for the grafana login
+
+Default value is "grafana"
+
+### grafana_config_root_url
+The url for grafana which is used in the grafana config file
+
+Default value is "http://{{ grafana_url }}"
+
+### grafana_http_port
+The port on which grafana should listen
+
+Default value is 3000
---
+++ roles/grafana/molecule/default/tests/test_default.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+def test_grafana_is_installed(host):
+    assert host.package("grafana").is_installed
+
+
+def test_grafana_is_running(host):
+    assert host.service("grafana-server").is_running
+
+
+@pytest.mark.parametrize("config", [
+    "admin_user = grafana",
+    "admin_password = grafana"
+])
+def test_config(host, config):
+    assert host.file("/etc/grafana/grafana.ini").contains(config)
+
+
+def test_config_root_url(host):
+    file = host.file("/etc/grafana/grafana.ini")
+    assert file.contains("root_url = http://debian_buster.local",) \
+        or file.contains("root_url = http://ubuntu_focal_fossa.local",)
+
+
+def test_listening_on_port(host):
+    assert host.socket("tcp://0.0.0.0:3000").is_listening
