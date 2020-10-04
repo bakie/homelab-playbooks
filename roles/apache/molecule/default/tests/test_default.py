d9e40d6740f2a9e5488fc70cb3fb8c966cc0b748
Bakie <bakelants.developer@gmail.com>
2020-10-04 12:51:29 +0200

    added apache role

---
+++ roles/apache/molecule/default/tests/test_default.py
@@ -0,0 +1,64 @@
+import pytest
+
+
+def test_apache_is_installed(host):
+    assert host.package("apache2").is_installed
+
+
+def test_apache_is_running(host):
+    assert host.service("apache2").is_running
+
+
+@pytest.mark.parametrize("name", [
+    "autoindex",
+    "deflate",
+    "expires",
+    "filter",
+    "headers",
+    "include",
+    "mime",
+    "rewrite",
+    "setenvif",
+    "ssl",
+    "proxy_html",
+    "proxy_http",
+    "xml2enc"
+])
+def test_modules_are_enabled(host, name):
+    assert host.file("/etc/apache2/mods-enabled/" + name + ".load").exists
+
+
+@pytest.mark.parametrize("name", [
+    "hostname",
+    "security"
+])
+def test_configs_are_enabled(host, name):
+    assert host.file("/etc/apache2/conf-enabled/" + name + ".conf").exists
+
+
+@pytest.mark.parametrize("value", [
+    "ServerTokens Prod",
+    "ServerSignature Off"
+])
+def test_security_conf_settings(host, value):
+    assert host.file("/etc/apache2/conf-available/security.conf").contains(value)
+
+
+def test_ssl_settings(host):
+    assert host.file("/etc/apache2/mods-available/ssl.conf").contains("SSLCipherSuite HIGH:!aNULL:!SHA1")
+
+
+@pytest.mark.parametrize("listen", [
+    "Listen 0.0.0.0:80",
+    "Listen 0.0.0.0:443"
+])
+def test_apache_listen_ports(host, listen):
+    assert host.file("/etc/apache2/ports.conf").contains(listen)
+
+
+@pytest.mark.parametrize("port", [
+    "80",
+    "443"
+])
+def test_listening_on_port(host, port):
+    assert host.socket("tcp://0.0.0.0:"+port).is_listening
