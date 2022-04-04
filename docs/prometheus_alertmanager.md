33c34b506d2f574b7ba7de5335c939a78a871250
Bakie <bakelants.developer@gmail.com>
2022-04-04 14:40:13 +0200

    refactor prometheus_alertmanager + lint fixes

--- docs/prometheus_alertmanager.md
+++ docs/prometheus_alertmanager.md
@@ -5,8 +5,6 @@ Install and configure prometheus alertmanager on the remote hosts.
 none
 
 ## Role variables
-| Variable                      | Default                        | Comments |
-|-------------------------------|--------------------------------|----------|
-| prometheus_alertmanager_group | "alertmanager"                 |          |
-| prometheus_alertmanager_user  | "alertmanager"                 |          |
-| prometheus_alertmanager_home  | "/opt/prometheus_alertmanager" |          |
+| Variable                              | Default                                        | Comments                                            |
+|---------------------------------------|------------------------------------------------|-----------------------------------------------------|
+| prometheus_alertmanager_slack_api_url | "http://prometheus_alertmanager_slack_api_url" | Slack api url where the alertmanager should post to |
--- molecule/prometheus_alertmanager/tests/test_prometheus.py
+++ molecule/prometheus_alertmanager/tests/test_prometheus.py
@@ -1,7 +1,15 @@
 import pytest
 import yaml
 
-PROMETHEUS_ALERTMANAGER_HOME = "/opt/prometheus_alertmanager"
+
+PROMETHEUS_ALERTMANAGER_USER = "prometheus_alertmanager"
+PROMETHEUS_ALERTMANAGER_GROUP = "prometheus_alertmanager"
+PROMETHEUS_ALERTMANAGER_PATH = "/opt/prometheus/alertmanager"
+PROMETHEUS_ALERTMANAGER_BASE_PATH = "/opt/prometheus"
+PROMETHEUS_ALERTMANAGER_CONFIG_PATH = "/etc/prometheus/alertmanager"
+PROMETHEUS_ALERTMANAGER_BASE_CONFIG_PATH = "/etc/prometheus"
+PROMETHEUS_ALERTMANAGER_DATA_PATH = "/opt/prometheus/alertmanager_data"
+PROMETHEUS_ALERTMANAGER_AMTOOL_CONFIG_PATH = "/etc/amtool"
 
 
 @pytest.fixture()
@@ -10,37 +18,60 @@ def ansible_defaults():
         return yaml.full_load(stream)
 
 
-@pytest.mark.parametrize("file", [
-    "/etc/alertmanager/alertmanager.yml",
-    "/etc/amtool/config.yml",
-    PROMETHEUS_ALERTMANAGER_HOME + "/alertmanager",
-    PROMETHEUS_ALERTMANAGER_HOME + "/amtool",
-    "/lib/systemd/system/alertmanager.service",
+def test_user_is_in_prometheus_alertmanager_group(host):
+    assert PROMETHEUS_ALERTMANAGER_GROUP in host.user(PROMETHEUS_ALERTMANAGER_USER).groups
+
+
+@pytest.mark.parametrize("path, owner", [
+    (PROMETHEUS_ALERTMANAGER_PATH, PROMETHEUS_ALERTMANAGER_USER),
+    (PROMETHEUS_ALERTMANAGER_BASE_PATH, "root"),
+    (PROMETHEUS_ALERTMANAGER_CONFIG_PATH, PROMETHEUS_ALERTMANAGER_USER),
+    (PROMETHEUS_ALERTMANAGER_BASE_CONFIG_PATH, "root"),
+    (PROMETHEUS_ALERTMANAGER_DATA_PATH, PROMETHEUS_ALERTMANAGER_USER)
 ])
-def test_files(host, file):
-    f = host.file(file)
-    assert f.exists
-    assert f.is_file
+def test_directory_owner(host, path, owner):
+    assert host.file(path).user == owner
 
 
-def test_user(host):
-    assert host.group("alertmanager").exists
-    assert host.user("alertmanager").exists
+@pytest.mark.parametrize("path, group", [
+    (PROMETHEUS_ALERTMANAGER_PATH, PROMETHEUS_ALERTMANAGER_GROUP),
+    (PROMETHEUS_ALERTMANAGER_BASE_PATH, "root"),
+    (PROMETHEUS_ALERTMANAGER_CONFIG_PATH, PROMETHEUS_ALERTMANAGER_GROUP),
+    (PROMETHEUS_ALERTMANAGER_BASE_CONFIG_PATH, "root"),
+    (PROMETHEUS_ALERTMANAGER_DATA_PATH, PROMETHEUS_ALERTMANAGER_GROUP)
+])
+def test_directory_group(host, path, group):
+    assert host.file(path).group == group
 
 
-def test_service_is_running(host):
-    assert host.service("alertmanager").is_running
+@pytest.mark.parametrize("path", [
+    PROMETHEUS_ALERTMANAGER_PATH,
+    PROMETHEUS_ALERTMANAGER_BASE_PATH,
+    PROMETHEUS_ALERTMANAGER_CONFIG_PATH,
+    PROMETHEUS_ALERTMANAGER_BASE_CONFIG_PATH,
+    PROMETHEUS_ALERTMANAGER_DATA_PATH
+])
+def test_directory_permissions(host, path):
+    assert host.file(path).mode == 0o755
 
 
-def test_service_is_enabled(host):
-    assert host.service("alertmanager").is_enabled
-
-
-def test_socket(host):
-    assert host.socket("tcp://127.0.0.1:9093").is_listening
+def test_amtool_config_file_path(host):
+    assert host.file(PROMETHEUS_ALERTMANAGER_AMTOOL_CONFIG_PATH+"/config.yml").contains("alertmanager.url: http://127.0.0.1:9093")
 
 
 def test_version(host, ansible_defaults):
     version = ansible_defaults['prometheus_alertmanager_version']
-    out = host.run(PROMETHEUS_ALERTMANAGER_HOME + "/alertmanager --version").stdout
+    out = host.run(PROMETHEUS_ALERTMANAGER_PATH + "/alertmanager --version").stdout
     assert "alertmanager, version " + version in out
+
+
+def test_prometheus_alertmanager_service_is_running(host):
+    assert host.service("prometheus_alertmanager").is_running
+
+
+def test_prometheus_alertmanager_service_is_enabled(host):
+    assert host.service("prometheus_alertmanager").is_enabled
+
+
+def test_listening_on_port(host):
+    assert host.socket("tcp://0.0.0.0:9093").is_listening
