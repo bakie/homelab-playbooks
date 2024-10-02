133a25f71563cdab56913f821a51da71d2cb802f
Bakie <bakelants.developer@gmail.com>
2024-10-02 21:10:53 +0200

    apache_vhosts document root

--- molecule/apache_vhosts/tests/test_apache_vhosts.py
+++ molecule/apache_vhosts/tests/test_apache_vhosts.py
@@ -2,29 +2,33 @@ import pytest
 
 
 @pytest.mark.parametrize("vhost", [
-    "molecule_http.conf",
-    "molecule_https.conf"
+    "molecule_proxy_pass_http.conf",
+    "molecule_proxy_pass_https.conf",
+    "molecule_document_root_http.conf",
+    "molecule_document_root_https.conf"
 ])
 def test_molecule_vhost_config_exists(host, vhost):
     assert host.file("/etc/apache2/sites-available/" + vhost).exists
 
 
 @pytest.mark.parametrize("vhost", [
-    "molecule_http.conf",
-    "molecule_https.conf"
+    "molecule_proxy_pass_http.conf",
+    "molecule_proxy_pass_https.conf",
+    "molecule_document_root_http.conf",
+    "molecule_document_root_https.conf"
 ])
 def test_molecule_vhost_config_symlink(host, vhost):
     assert host.file("/etc/apache2/sites-enabled/" + vhost).linked_to == "/etc/apache2/sites-available/" + vhost
 
 
 @pytest.mark.parametrize("config", [
-    "ServerName molecule_http.random.url",
+    "ServerName molecule_proxy_pass_http.random.url",
     "ProxyPass / http://0.0.0.0:9003",
     "ProxyPassReverse / http://0.0.0.0:9003",
-    "CustomLog ${APACHE_LOG_DIR}/molecule_http.access.log log"
+    "CustomLog ${APACHE_LOG_DIR}/molecule_proxy_pass_http.access.log log"
 ])
-def test_molecule_http_vhost(host, config):
-    assert host.file("/etc/apache2/sites-available/molecule_http.conf").contains(config)
+def test_molecule_proxy_pass_http_vhost(host, config):
+    assert host.file("/etc/apache2/sites-available/molecule_proxy_pass_http.conf").contains(config)
 
 
 @pytest.mark.parametrize("config", [
@@ -32,8 +36,8 @@ def test_molecule_http_vhost(host, config):
     "SSLCertificateFile",
     "SSLCertificateKeyFile"
 ])
-def test_molecule_http_does_not_contain_https_config(host, config):
-    assert not host.file("/etc/apache2/sites-available/molecule_http.conf").contains(config)
+def test_molecule_proxy_pass_http_does_not_contain_https_config(host, config):
+    assert not host.file("/etc/apache2/sites-available/molecule_proxy_pass_http.conf").contains(config)
 
 
 @pytest.mark.parametrize("config", [
@@ -41,5 +45,33 @@ def test_molecule_http_does_not_contain_https_config(host, config):
     "SSLCertificateFile",
     "SSLCertificateKeyFile"
 ])
-def test_molecule_https_contains_https_config(host, config):
-    assert host.file("/etc/apache2/sites-available/molecule_https.conf").contains(config)
+def test_molecule_proxy_pass_https_contains_https_config(host, config):
+    assert host.file("/etc/apache2/sites-available/molecule_proxy_pass_https.conf").contains(config)
+
+
+@pytest.mark.parametrize("config", [
+    "ServerName molecule_document_root_http.random.url",
+    "DocumentRoot /var/www/html/",
+    "Directory /var/www/html/",
+    "CustomLog ${APACHE_LOG_DIR}/molecule_document_root_http.access.log log"
+])
+def test_molecule_document_root_http_vhost(host, config):
+    assert host.file("/etc/apache2/sites-available/molecule_document_root_http.conf").contains(config)
+
+
+@pytest.mark.parametrize("config", [
+    "<VirtualHost \\*:443>",
+    "SSLCertificateFile",
+    "SSLCertificateKeyFile"
+])
+def test_molecule_document_root_http_does_not_contain_https_config(host, config):
+    assert not host.file("/etc/apache2/sites-available/molecule_document_root_http.conf").contains(config)
+
+
+@pytest.mark.parametrize("config", [
+    "<VirtualHost \\*:443>",
+    "SSLCertificateFile",
+    "SSLCertificateKeyFile"
+])
+def test_molecule_document_root_https_contains_https_config(host, config):
+    assert host.file("/etc/apache2/sites-available/molecule_document_root_https.conf").contains(config)
