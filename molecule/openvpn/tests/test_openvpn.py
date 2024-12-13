b3367ffa6963db4ba12e16e286fa938ac53fdcd8
Bakie <bakelants.developer@gmail.com>
2024-12-13 19:14:42 +0100

    change openvpn config

--- molecule/openvpn/tests/test_openvpn.py
+++ molecule/openvpn/tests/test_openvpn.py
@@ -14,15 +14,20 @@ def test_default(host):
 
 
 def test_openvpn_testName_is_enabled(host):
-    assert host.service("openvpn@testName.service").is_enabled
+    assert host.service("openvpn@dummy.service").is_enabled
+
+
+def test_openvpn_auth_user_pass_file_path(host):
+    assert host.file("/etc/openvpn/auth_user_pass").mode == 0o600
 
 
 @pytest.mark.parametrize("setting", [
-    "remote testRemote 8000",
-    "randomCa",
-    "randomCert",
-    "randomKey",
-    "randomTlsAuth"
+    "molecule_username",
+    "molecule_password"
 ])
-def test_openvpn_client_config(host, setting):
-    assert host.file("/etc/openvpn/testName.conf").contains(setting)
+def test_openvpn_auth_user_pass(host, setting):
+    assert host.file("/etc/openvpn/auth_user_pass").contains(setting)
+
+
+def test_openvpn_client_config(host):
+    assert host.file("/etc/openvpn/dummy.conf").contains("auth-user-pass /etc/openvpn/auth_user_pass")
